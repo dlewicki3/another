29a83707da5aca40af8972177a0f54b5112686d8
Dominik Lewicki <dominik_03-06@wp.pl>
2024-03-25 18:22:09 +0100

    ffff

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import O1 from "./components/O1.jsx";
+import O2 from "./components/O2.jsx";
+import O3 from "./components/O3.jsx";
 
 function App() {
   return (
     <body>
      <Navbar/>
      <O1/>
+     <O2/>
+     <O3/>
     </body>
   );
 }
---
+++ src/components/O2.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import  './O2.css';
+import I1 from './I1.svg';
+import I2 from './I2.svg';
+import I3 from './I3.svg';
+import I4 from './I4.svg';
+import I5 from './I5.svg';
+import I6 from './I6.svg';
+import I7 from './I7.svg';
+import I8 from './I8.svg';
+
+const O2 = () => {
+  return (
+<div className="o2">
+    <div className="k2">
+        <p className='small-txt2'>Lorem ipsum dolor sit amet consectetur<br></br> adipisicing elit. Id!</p>
+<div className="top-bottom">
+    <div className="top2">
+        <div className="kwa2">
+            <img className='foto2' src= {I1}></img>
+        </div>
+        <div className="kwa2">
+        <img className='foto2' src= {I2}></img>
+        </div>
+        <div className="kwa2">
+        <img className='foto2' src= {I3}></img>
+        </div>
+        <div className="kwa2">
+        <img className='foto2' src= {I4}></img>
+        </div>
+    </div>
+    <div className="bottom2">
+    <div className="kwa2">
+        <img className='foto2' src= {I5}></img>
+        </div>
+        <div className="kwa2">
+        <img className='foto2' src= {I6}></img>
+        </div>
+        <div className="kwa2">
+        <img className='foto2' src= {I7}></img>
+        </div>
+        <div className="kwa2">
+        <img className='foto2' src= {I8}></img>
+        </div>
+    </div>
+</div>
+<div className="extra-thin-div"></div>
+    </div>
+</div>
+  )
+}
+
+export default O2
