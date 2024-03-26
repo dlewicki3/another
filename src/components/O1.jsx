94e6fbbdaa24f0da4df5c6b403eb0cbae604be5a
Dominik Lewicki <dominik_03-06@wp.pl>
2024-03-26 18:54:18 +0100

    ffff

--- src/components/O1.jsx
+++ src/components/O1.jsx
@@ -1,33 +1,32 @@
-import React from 'react'
+import React from 'react';
 import './O1.css';
 import { ReactTyped } from "react-typed";
-
 import { FaArrowRight } from "react-icons/fa6";
 import Komp1 from './Komp1.png';
 
 const O1 = () => {
   return (
-   <div className="o1">
-    <div className="k1">
-      <div className="top1">
-<p className='big-txt1'>Your
-<span className='span-edit'>AI</span> 
-
-knowledge<br></br> <ReactTyped strings ={['Friend', 'Specialist', 'Teacher']} typeSpeed={120} backSpeed={140}   loop className='type'/> </p>
-
-<p className='small-txt1'>Get ready-to-use answers from all your knowledge<br></br> and quit manual organization for good.</p>
-<button className='btn1'>JOIN TO THE WAITLIST <FaArrowRight />
-</button>
-</div>
-<div className="komp">
-  <img className='icon' src = {Komp1}></img>
-</div>
-
+    <div className="o1">
+      <div className="k1">
+        <div className="top1">
+          <p className='big-txt1'>
+            Your <span className='span-edit'>AI</span> knowledge<br></br> 
+            <ReactTyped strings={['Friend', 'Specialist', 'Teacher']} 
+              typeSpeed={80} backSpeed={100} loop className='type' />
+          </p>
+          <p className='small-txt1'>
+            Get ready-to-use answers from all your knowledge<br></br> 
+            and quit manual organization for good.
+          </p>
+          <button className='btn1'>JOIN TO THE WAITLIST <FaArrowRight /></button>
+        </div>
+        <div className="komp">
+          <img className='icon' src={Komp1} alt="icon" />
+        </div>
+      </div>
+      <div className="absolute1">efwefew</div>
     </div>
-    <div className="absolute1">efwefew</div>
-   </div>
-   
-  )
+  );
 }
 
-export default O1
+export default O1;
